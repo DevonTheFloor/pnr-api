bb4d11a9a66db1b9503e69775ef1d267aa6e1840
DevonTheFloor <devonthefloor@free.fr>
2022-07-03 13:51:26 +0200

    All minify

--- serv-pnr-api.js
+++ serv-pnr-api.js
@@ -1,10 +1,1 @@
-const http = require('http'),
-api = require('./api'),
-apiPort = process.env.PNR_API_PORT,
-apiHost = process.env.PNR_API_HOSTNAME; 
-
-api.set('port', apiPort);
-const server = http.createServer(api);
-server.listen(apiPort, apiHost);
-console.log('Pnr API listen port : ' + apiPort + ' on host : '+ apiHost)
-
+const http=require("http"),api=require("./api"),apiPort=process.env.PNR_API_PORT,apiHost=process.env.PNR_API_HOSTNAME;api.set("port",apiPort);const server=http.createServer(api);server.listen(apiPort,apiHost),console.log("Pnr API listen port : "+apiPort+" on host : "+apiHost);
